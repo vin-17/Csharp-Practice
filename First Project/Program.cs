ef09827d6287e1f0150a9a1af21495dbdbe6f870
Vineet Singh <vineet.yo3@gmail.com>
2024-06-24 15:21:29 +0530

    Add project files.

---
+++ First Project/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(" enter the number : ");
+            string input = Console.ReadLine();
+
+            Boolean success = false;
+            while (!success)
+            {
+                if(int.TryParse(input, out int num))
+                {
+                    success = true;
+                    for(int i=1; i<=10; i++)
+                    {
+                        Console.WriteLine(num * i);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("please provide and integer !! ");
+                    Console.Write(" enter the number : ");
+                    input = Console.ReadLine();
+                }
+            }
+
+        }
+    }
+}
